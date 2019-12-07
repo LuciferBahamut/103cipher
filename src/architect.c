86650fee95e338ef90e23fdb7a4caeda2f0e14cd
Guerlain Blanchard <gblanchard@desktop-8lpiv0t.home>
2019-12-07 13:02:09 -0500

    [ADD] calculatrice de matrice [ADD] createur de matrice-clef a partir d'une clef

---
+++ src/architect.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2019
+** architect
+** File description:
+** arch
+*/
+
+#include "my.h"
+
+float calc_multi(float **f, float **key, int i, int j)
+{
+    float r = 0;
+    
+    for (int s = 0; s < get_size(key); s++)
+        r += f[i][s] * key[s][j];
+    return (r);
+}
+
+float **mult_architech(float **f, float **key, int size)
+{
+    float **r = create_matrix_e(size);
+
+    for (int i = 0; i < get_size(f); i++) {
+        for (int j = 0; j < size; j++) {
+            r[i][j] = calc_multi(f, key, i, j);
+        }
+    }
+    return (r);
+}
---
+++ src/make_key.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2019
+** make_key
+** File description:
+** key
+*/
+
+#include <stdlib.h>
+
+int make_size(char *key)
+{
+    int i = my_strlen(key);
+    int j = 0;
+
+    while (i > 0)
+    {
+        i /= 2;
+        j++;
+    }
+    return (j);
+}
+
+float **fill_key(float **f, char *key, int size)
+{
+    int i = 0;
+    char c;
+
+    for (int j = 0; j < size; j++) {
+        for (int k = 0; k < size; k++) {
+            if (key[i] != '\0') {
+                c = key[i];
+                f[j][k] = c;
+                i++;
+            }
+            else 
+                f[j][k] = 0;
+        }
+    }
+    return (f);
+}
+
+float **make_key(char *key)
+{
+    int i = make_size(key);
+    float **f = malloc(sizeof(float*) * i);
+
+    for (int j = 0; j < i; j++)
+        f[j] = malloc(i);
+    return (fill_key(f, key, i));
+}
