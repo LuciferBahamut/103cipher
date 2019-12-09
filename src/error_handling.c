2acaeda1b736fc7308e1309c795c74a14e8937d5
Quentin TREHEUX <qtreheux@localhost.localdomain>
2019-12-09 14:25:00 +0100

    [MOV] architect in src/ [MOD] error_handling for all error (normalement)

--- src/error_handling.c
+++ src/error_handling.c
@@ -9,8 +9,10 @@
 
 int error_handling(int ac, char **av)
 {
-    if (av[1][0] == '-' && av[1][1] == 'h')
-        return (0);
+    if (ac == 2) {
+        if (av[1][0] == '-' && av[1][1] == 'h')
+            return (0);
+    }
     if (ac != 4)
         return (1);
     if (av[3][0] != '0' && av[3][0] != '1')
